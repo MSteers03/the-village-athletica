26addc3e8379dffc7949bf329ad46c92203c9cf9
Mitch <mitchellsteers@gmail.com>
2025-09-24 22:27:07 +0800

    Correcting contact to email for testing

--- app/Http/Controllers/MaintenanceController.php
+++ app/Http/Controllers/MaintenanceController.php
@@ -25,7 +25,7 @@ class MaintenanceController extends Controller
         
         try {
             // Send email using Resend
-            Mail::to('village@steersfam.com')
+            Mail::to('mitchellsteers@gmail.com')
                 ->send(new MaintenanceContactForm($request->all()));
             
             return redirect()->back()->with('success', 'Thank you! We have received your message and will contact you soon.');
