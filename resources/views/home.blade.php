9dd5f3d30e3b0fa2a446ca5bc552db8a1235f266
Mitch <84167964+MSteers03@users.noreply.github.com>
2026-01-15 23:42:31 +0800

    Feature/home page (#9)
    * Add contact us form email logic
    
    * Adding Recaptcha logic
    
    * Updating Github test
    
    * Removing double up recaptcha T's & C's
    
    * Updating home page
    
    * Updating home site title
    
    * Updating package and deploying Cloudinary

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,10 +74,10 @@
             <!-- Right Column - Image Placeholder -->
             <div class="bg-gray-300 rounded-2xl aspect-[3/4] flex items-center justify-center">
                 <div class="text-center text-gray-600">
-                    <svg class="w-24 h-24 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <!--<svg class="w-24 h-24 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                    </svg>
-                    <p class="text-lg">Training Image</p>
+                    </svg>-->
+                    <x-cloudinary::image public-id="headshot_ftizhb" width="1000" height="10" />
                 </div>
             </div>
         </div>
